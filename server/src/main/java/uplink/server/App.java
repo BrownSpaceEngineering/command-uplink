70b81dcafd1ae6e2e6a934422eb8a8b66b90b8c5
Arvind Yalavarti <yalavartiarvind@gmail.com>
2018-10-28 21:21:47 -0400

    Add Spring dependency to pom.xml file

--- server/src/main/java/uplink/server/App.java
+++ server/src/main/java/uplink/server/App.java
@@ -1,13 +1,30 @@
 package uplink.server;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import uplink.server.services.HelloWorldService; 
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	@SuppressWarnings("resource")
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		//Load definition from given XML file
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		
+		HelloWorldService service = (HelloWorldService) context.getBean("helloWorldService");
+		
+		//Print default name
+		String message = service.sayHello();
+		System.out.println(message);
+		
+		//Set a new name
+		service.setName("Uplink Subgroup");
+		message = service.sayHello();
+		System.out.println(message);
     }
 }
