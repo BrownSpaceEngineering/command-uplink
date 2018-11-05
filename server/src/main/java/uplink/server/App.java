583e20a3bb0ef41db285395d63040fe687d75c09
Arvind Yalavarti <yalavartiarvind@gmail.com>
2018-11-04 21:13:15 -0500

    Add Spring-Boot dependencies and create a \hello route

--- server/src/main/java/uplink/server/App.java
+++ server/src/main/java/uplink/server/App.java
@@ -1,29 +1,12 @@
 package uplink.server;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext; 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-	@SuppressWarnings("resource")
-    public static void main( String[] args )
-    {
-		//Load definition from given XML file
-		ApplicationContext context = new ClassPathXmlApplicationContext(
-				"applicationContext.xml");
-		
-		HelloWorldService service = (HelloWorldService) context.getBean("helloWorldService");
-		
-		//Print default name
-		String message = service.sayHello();
-		System.out.println(message);
-		
-		//Set a new name
-		service.setName("Uplink Subgroup");
-		message = service.sayHello();
-		System.out.println(message);
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
